1a256fa1a2d133773ca88c7ef152ac6335f0cbe0
mattsedlar <sedlarm@gmail.com>
2017-01-23 20:32:02 -0500

    Action page updated text and styles

--- form/models.py
+++ form/models.py
@@ -4,18 +4,18 @@ from django.db import models
 
 # Create your models here.
 class Response(models.Model):
-  unique_key = models.CharField(default='',max_length=8)
-  position = models.CharField(default='',max_length=100)
-  facility = models.CharField(default='',max_length=100)
-  location = models.CharField(default='',max_length=100)
-  clinic_name = models.CharField(default='',max_length=100)
+  unique_key = models.CharField(null=True,max_length=8)
+  position = models.CharField(null=True,max_length=100)
+  facility = models.CharField(null=True,max_length=100)
+  location = models.CharField(null=True,max_length=100)
+  clinic_name = models.CharField(null=True,max_length=100)
   opening = models.IntegerField(default=0)
   mid = models.IntegerField(default=0)
   breaks = models.IntegerField(default=0)
   sick = models.IntegerField(default=0)
   closing = models.IntegerField(default=0)
-  name = models.CharField(default='',max_length=100)
-  email = models.CharField(default='',max_length=100)
+  name = models.CharField(null=True,max_length=100)
+  email = models.CharField(null=True,max_length=100)
   
   def __str__(self):
     return self.name + ": " +self.position + ", " + self.facility
