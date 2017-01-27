46ffc39099447f263e89ffbf8d2cc8d820afc92e
mattsedlar <sedlarm@gmail.com>
2017-01-27 11:00:42 -0500

    Style updates to resemble home site

--- form/static/js/scripts.js
+++ form/static/js/scripts.js
@@ -1,5 +1,5 @@
 let step = 0;
-let colors = ['#e3f0f7','#b5c0c4','#93aeb6','#93aeb6','#b5c0c4','#e3f0f7'];
+// let colors = ['#e3f0f7','#b5c0c4','#93aeb6','#93aeb6','#b5c0c4','#e3f0f7'];
 
 $(document).ready(function(){
   // Steps
@@ -7,7 +7,7 @@ $(document).ready(function(){
     console.log('#btnStep' + step);
     $('#step' + step).css('display','none');
     step += 1;
-    $('body').css('background-color',colors[step]);
+    //$('body').css('background-color',colors[step]);
     $('#step'+ step).css('display','block');
   });
   // Prevent Zero Values for Shifts
